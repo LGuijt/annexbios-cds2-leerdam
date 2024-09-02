763db3a11b5ab42f6e049cb37eedeffea7ca7dcc
LGuijt <mail.lisageurts@gmail.com>
2024-09-02 12:35:07 +0200

    basis mappenstructuur

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+$view = '';
+
+if (!empty($_GET['view'])) {
+    $view = htmlspecialchars($_GET['view']);
+    $view = str_replace("/", "", $view);
+}
+
+$page = '';
+$style = '';
+$js = '';
+
+switch ($view) {
+    case '':
+        $page = 'home.php';
+        $style = 'home.css';
+        $js = 'home.js';
+        break;
+    case 'agenda':
+        $page = 'agenda.php';
+        $style = 'agenda.css';
+        $js = 'agenda.js';
+        break;
+    case 'bestel':
+        $page = 'bestel.php';
+        $style = 'bestel.css';
+        $js = 'bestel.js';
+        break;
+    case 'film':
+        $page = 'film.php';
+        $style = 'film.css';
+        $js = 'film.js';
+        break;
+    default:
+        $page = '404.php';
+        $style = '404.css';
+        $js = '404.js';
+        break;
+}
+
+require_once "./views/". $page;
