1d5bb53f2cb314866d2822bcd193ccccaa6d433e
LGuijt <mail.lisageurts@gmail.com>
2024-09-03 16:13:50 +0200

    footer helft af

--- core/footer.php
+++ core/footer.php
@@ -1,6 +1,25 @@
 <div id="mainFooter">
         <div id="upperHalf2">
-            <img src="./assets/img/movietrack.png" alt="MovieTrack Logo" id="footerLogo">
+            <div id="footerinfo">
+                <img src="./assets/img/logotwee.png" alt="logo">
+                <div>Lorem Ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,</div>
+                <a>LEES MEER</a>
+            </div>
+            <div id="footernav">
+                <div id="navtop">NAVIGEER</div>
+                <div id="navlinks">
+                <a href="#">Werken bij</a>
+                <a href="#">Veelgestelde vragen</a>
+                <a href="#">Vestigingen</a>
+                <a href="#">Contact</a>
+                </div>
+            </div>
+            <div id="footerSocial">
+                <a href="#"><img src="assets/img/facebook.png" alt="facebook"></a>
+                <a href="#"><img src="assets/img/twitter.png" alt="twitter"></a>
+                <a href="#"><img src="assets/img/instagram.png" alt="instagram"></a>
+            </div>
+            <img id="track" src="./assets/img/movietrack.png" alt="MovieTrack Logo" id="footerLogo">
         </div>
         <div id="lowerHalf2"></div>
     </div>
