3d4d6b6deb1b83fd200018142603e71552e94db2
Rick <144334803+8-Click@users.noreply.github.com>
2024-09-03 14:39:08 +0200

    eerste box is af en nog wat andere dingetejs

--- views/home.php
+++ views/home.php
@@ -1,11 +1,15 @@
 <?php include 'core/header.php'; ?>
 
-<div id="parentContainer">
-    <div id="opacityBox"></div>
-    <div id="welcomeBox"></div>
+<div id="parentContainer" style="margin-top:30vh;">
+    <div id="imageContainer">
+        <div id="opacityBox"></div>
+    </div>
+    <div id="welcomeBox">
+        <h1>WELKOM BIJ ANNEXBIOS LEERDAM</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam, dolore facilis. Quisquam commodi veniam, eius fugit ab exercitationem labore eum sit ullam eos reiciendis vitae ipsum corrupti inventore rerum distinctio.</p>
+        <div id="referButton"></div>
+    </div>
     <div id="locationBox"></div>
 </div>
-
-
 
 <?php include 'core/footer.php'; ?>
