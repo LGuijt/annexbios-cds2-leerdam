faeeea5ffbd5d4f5ec56ac5a7cb02bf9b2985ca9
Kena1Y <kenai.e.yard@gmail.com>
2024-09-09 11:27:51 +0200

    test

---
+++ views/film.php
@@ -0,0 +1,12 @@
+
+<?php include './core/header.php'; ?>
+
+<div class="filmpagina">
+<div id="titlebox">
+    <h1 style="font-size:3em;">JURASSIC WORLD: FALLEN KINGDOM</h1>
+</div>
+
+</div>
+
+
+<?php include './core/footer.php'; ?>
