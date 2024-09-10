8d32cbf2c5d967850ee1327b13a4e5cdc1611f1c
Kena1Y <kenai.e.yard@gmail.com>
2024-09-10 16:16:00 +0200

    layout finished

--- views/film.php
+++ views/film.php
@@ -3,10 +3,36 @@
 
 <div class="filmpagina">
 <div id="titlebox">
-    <h1 style="font-size:3em;">JURASSIC WORLD: FALLEN KINGDOM</h1>
+    <h1 style="font-size:3em; font-weight: 300; padding:2vw;">JURASSIC WORLD: FALLEN KINGDOM</h1>
+</div>
+<div id="movieposter">
+<img src="./assets/img/Dino.png" height="100%" width="633vh">
 </div>
 
+<div id="movieinfo">
+<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 2vw;">
+<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
+<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
+<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
+<img src="./assets/img/emptystar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
+
+<div id="kw">
+<img src="./assets/img/kijkwijzer-12.png" height="50" width="50" style="padding: 1vw 0 0 2vw;">
+<img src="./assets/img/kijkwijzer-eng.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
+<img src="./assets/img/kijkwijzer-geweld.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
+<h1 style="font-size:2em; font-weight: 400; padding: 0 1vw 0.5vw 2vw; color:#666666;" id="releasedate">Release:</h1>
+<p style="font-size:1.3em; font-weight: 300;padding: 0 0 20vw 2vw; color:#666666;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus id fuga eius autem magni ipsam repellat corrupti? Harum, ducimus voluptas laudantium ipsa nesciunt dolores, veniam alias commodi accusantium illo minus Lorem Lorem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus eaque minus modi voluptates nostrum natus unde rem! Placeat magni eum facilis dolor dignissimos perferendis, nam officiis! Officiis inventore nam tempore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum libero ex maxime, non eaque culpa. Dolores, deleniti consequatur repellat fuga illum dolor obcaecati corporis! Eos deleniti odio culpa animi! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate ad natus praesentium laudantium hic. <p/>
 </div>
+
+
+</div>
+
+<div id="ticketbox"><h1 style="font-size:2.5em; font-weight: 300;">KOOP JE TICKETS</h1></div>
+
+<div id="movietrailer"></div>
+
+</div>
+
 
 
 <?php include './core/footer.php'; ?>
