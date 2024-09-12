7484603051014ae4482aac82c169a02f05c4d83b
Kena1Y <kenai.e.yard@gmail.com>
2024-09-12 14:29:08 +0200

    meer info

--- views/film.php
+++ views/film.php
@@ -10,18 +10,27 @@
 </div>
 
 <div id="movieinfo">
-<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 2vw;">
-<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
-<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
-<img src="./assets/img/fullstar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
-<img src="./assets/img/emptystar.png" height="50" width="50" style="padding: 2vw 0 0 0;">
+<img class="starImage" src="./assets/img/fullstar.png" style="padding: 2vw 0 0 2vw;">
+<img class="starImage" src="./assets/img/fullstar.png">
+<img class="starImage" src="./assets/img/fullstar.png">
+<img class="starImage" src="./assets/img/fullstar.png">
+<img class="starImage" src="./assets/img/emptystar.png">
 
-<div id="kw">
-<img src="./assets/img/kijkwijzer-12.png" height="50" width="50" style="padding: 1vw 0 0 2vw;">
-<img src="./assets/img/kijkwijzer-eng.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
-<img src="./assets/img/kijkwijzer-geweld.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
-<h1 style="font-size:2em; font-weight: 400; padding: 0 1vw 0.5vw 2vw; color:#666666;" id="releasedate">Release:</h1>
-<p style="font-size:1.3em; font-weight: 300;padding: 0 0 20vw 2vw; color:#666666;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus id fuga eius autem magni ipsam repellat corrupti? Harum, ducimus voluptas laudantium ipsa nesciunt dolores, veniam alias commodi accusantium illo minus Lorem Lorem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus eaque minus modi voluptates nostrum natus unde rem! Placeat magni eum facilis dolor dignissimos perferendis, nam officiis! Officiis inventore nam tempore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum libero ex maxime, non eaque culpa. Dolores, deleniti consequatur repellat fuga illum dolor obcaecati corporis! Eos deleniti odio culpa animi! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate ad natus praesentium laudantium hic. <p/>
+<div id="kijkwijzer">
+    <img src="./assets/img/kijkwijzer-12.png" height="50" width="50" style="padding: 1vw 0 0 2vw;">
+    <img src="./assets/img/kijkwijzer-eng.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
+    <img src="./assets/img/kijkwijzer-geweld.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
+   
+    <h1 id="releasedate">Release:</h1>
+    <p style="font-size:1.3em; font-weight: 300;padding: 0vw 1vw 0.5vw 2vw; color:#666666;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus id fuga eius autem magni ipsam repellat corrupti? Harum, ducimus voluptas laudantium ipsa nesciunt dolores, veniam alias commodi accusantium illo minus Lorem Lorem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus eaque minus modi voluptates nostrum natus unde rem! Placeat magni eum facilis dolor dignissimos perferendis, nam officiis! Officiis inventore nam tempore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum libero ex maxime, non eaque culpa. Dolores, deleniti consequatur repellat fuga illum dolor obcaecati corporis! Eos deleniti odio culpa animi! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate ad natus praesentium laudantium hic.<p/>
+    <h1 class="info">Genre:</h1>
+    <h1 class="info">Filmlengte:</h1>
+    <h1 class="info">Land:</h1>
+    <h1 class="info">Imdb score:</h1>
+    <h1 class="info">Regisseur:</h1>
+    <h1 class="info">Acteurs:</h1>
+    
+    
 </div>
 
 
