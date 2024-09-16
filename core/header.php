84dc976c8b255ba6b1d670ef9ad1eb5f8570b8b1
LGuijt <mail.lisageurts@gmail.com>
2024-09-16 11:42:20 +0200

    Update header.php

--- core/header.php
+++ core/header.php
@@ -34,36 +34,38 @@
 <body>
     <?php
 
-// $authorization = "Authorization: Bearer 2b8e7f9a3c1d5e4f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d";
-// $url = 'https://u231195.gluwebsite.nl/api/v1/movieData';
+$authorization = "Authorization: Bearer 2b8e7f9a3c1d5e4f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6a7b8c9d";
+$url = 'https://annexbios.nickvz.nl/api/v1/movieData';
 
-// // Initialize cURL session
-// $ch = curl_init($url);
+// Initialize cURL session
+$ch = curl_init($url);
 
-// // Set cURL options
-// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+// Set cURL options
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-// // Execute cURL request
-// $result = curl_exec($ch);
+// Execute cURL request
+$result = curl_exec($ch);
 
-// // Check for cURL errors
-// if ($result === false) {
-//     echo 'cURL Error: ' . curl_error($ch);
-// } else {
-//     // Check HTTP status code
-//     $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-//     echo 'HTTP Status Code: ' . $http_status . "\n";
-//     echo 'Response: ' . $result;
-// }
+// Check for cURL errors
+if ($result === false) {
+    echo 'cURL Error: ' . curl_error($ch);
+}
 
-// // Close cURL session
-// curl_close($ch);
+// Close cURL session
+curl_close($ch);
 
 
-//     var_dump($result);
+    // var_dump($result);
+    $res = json_decode($result, true);
+    // var_dump($res['data']);
+    $moviedata = $res['data'];
+    // var_dump($moviedata);
+    foreach ($moviedata as $movie) {
+        echo $movie['title'];
+    }
 
     $file_json = file_get_contents('assets/json/dummylocation.json');
     $locationdata = json_decode($file_json, true);
@@ -90,15 +92,6 @@
                 <select name="filmchoice" id="filmchoice">
                     <option>Kies je film</option>
                     <?php 
-                    foreach ($moviedata as $movie) {
-                        foreach ($locationdata as $location) {
-                            if ($location["id"] == 3 && $location["movie_id"] == $movie["api_id"]) {
-                                ?>
-                                <option value="<?= $movie['api_id']; ?>"><?= $movie['title']; ?></option>
-                                <?php
-                            }
-                        }
-                    }
                     ?>
                 </select>
                 <input type="submit" value="BESTEL TICKETS">
