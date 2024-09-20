c2a49ea963126c6b7a5d81ddf4c09f46617cc6e5
Kena1Y <kenai.e.yard@gmail.com>
2024-09-20 15:19:45 +0200

    finished

--- core/header.php
+++ core/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AnnexBios</title>
 
-    <link rel="icon" type="image/png" href="assets/img/popcornicon.png"
+    <link rel="icon" type="image/png" href="assets/img/popcornicon.png">
 
     <link rel="stylesheet" href="<?= 'assets/css/style.css' ?>">
     <link rel="stylesheet" href="<?= 'assets/css/header.css' ?>">
--- views/film.php
+++ views/film.php
@@ -1,54 +1,63 @@
 
+
 <?php include './core/header.php'; ?>
 
 <div class="filmpagina">
-<div id="titlebox">
-    <h1 style="font-size:3em; font-weight: 300; padding:2vw;">JURASSIC WORLD: FALLEN KINGDOM</h1>
-</div>
-<div id="movieposter">
-<img src="./assets/img/Dino.png" height="100%" width="633vh">
-</div>
+    <div id="titlebox">
+        <h1 id="titleID">JURASSIC WORLD: FALLEN KINGDOM</h1>
+    </div>
 
-<div id="movieinfo">
-<img class="starImage" src="./assets/img/fullstar.png" style="padding: 2vw 0 0 2vw;">
-<img class="starImage" src="./assets/img/fullstar.png">
-<img class="starImage" src="./assets/img/fullstar.png">
-<img class="starImage" src="./assets/img/fullstar.png">
-<img class="starImage" src="./assets/img/emptystar.png">
+    <div class="filmflexbox">
+        <div id="movieposter">
+            <img src="./assets/img/Dino.png" height="100%" width="100%">
+        </div>
+        <div class="allinfo" style="background-color: white">
+            <div id="movieinfo">
+                <img class="starImage" src="./assets/img/fullstar.png" style="padding: 2vw 0 0 2vw;">
+                <img class="starImage" src="./assets/img/fullstar.png">
+                <img class="starImage" src="./assets/img/fullstar.png">
+                <img class="starImage" src="./assets/img/fullstar.png">
+                <img class="starImage" src="./assets/img/emptystar.png">
+            </div>
 
-<div id="kijkwijzer">
-    <img src="./assets/img/kijkwijzer-12.png" height="50" width="50" style="padding: 1vw 0 0 2vw;">
-    <img src="./assets/img/kijkwijzer-eng.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
-    <img src="./assets/img/kijkwijzer-geweld.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
-   
-    <h1 id="releasedate">Release:</h1>
-    <p style="font-size:1.3em; font-weight: 300;padding: 0vw 1vw 0.5vw 2vw; color:#666666;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus id fuga eius autem magni ipsam repellat corrupti? Harum, ducimus voluptas laudantium ipsa nesciunt dolores, veniam alias commodi accusantium illo minus Lorem Lorem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus eaque minus modi voluptates nostrum natus unde rem! Placeat magni eum facilis dolor dignissimos perferendis, nam officiis! Officiis inventore nam tempore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum libero ex maxime, non eaque culpa. Dolores, deleniti consequatur repellat fuga illum dolor obcaecati corporis! Eos deleniti odio culpa animi! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate ad natus praesentium laudantium hic.<p/>
+
+            <div id="kijkwijzer" style="left: 4vw;">
+                <img src="./assets/img/kijkwijzer-12.png" height="50" width="50" style="padding: 1vw 0 0 2vw;">
+                <img src="./assets/img/kijkwijzer-eng.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
+                <img src="./assets/img/kijkwijzer-geweld.png" height="50" width="50" style="padding:  0 2vw 1vw vw;">
+
+                <h1 id="releasedate">Release:</h1>
+                <p style="font-size:1.3em; font-weight: 300;padding: 0vw 1vw 0.5vw 2vw; color:#666666;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus id fuga eius autem magni ipsam repellat corrupti? Harum, ducimus voluptas laudantium ipsa nesciunt dolores, veniam alias commodi accusantium illo minus Lorem Lorem. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus eaque minus modi voluptates nostrum natus unde rem! Placeat magni eum facilis dolor dignissimos perferendis, nam officiis! Officiis inventore nam tempore. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatum libero ex maxime, non eaque culpa. Dolores, deleniti consequatur repellat fuga illum dolor obcaecati corporis! Eos deleniti odio culpa animi! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate ad natus praesentium laudantium hic.
+                    <p />
+
+                <h1 class="info">Genre:</h1>
+                <h1 class="info">Filmlengte:</h1>
+                <h1 class="info">Land:</h1>
+                <h1 class="info">Imdb score:</h1>
+                <h1 class="info">Regisseur:</h1>
+                <h1 class="info">Acteurs:</h1>
+
+                <img class="actorpics" src="./assets/img/BryceDallas.png" style="padding: 0.5vw 0 0 2vw;"">
+                <img class=" actorpics" src="./assets/img/Chris_Pratt.png">
+                <img class="actorpics" src="./assets/img/rafe_spall.png">
+                <img class="actorpics" src="./assets/img/Toby_Jones.png">
+            </div>
+        </div>
+    </div>
     
-    <h1 class="info">Genre:</h1>
-    <h1 class="info">Filmlengte:</h1>
-    <h1 class="info">Land:</h1>
-    <h1 class="info">Imdb score:</h1>
-    <h1 class="info">Regisseur:</h1>
-    <h1 class="info">Acteurs:</h1>
-
-    <img class="actorpics" src="./assets/img/BryceDallas.png"  style="padding: 0.5vw 0 0 2vw;"">
-     <img class="actorpics" src="./assets/img/Chris_Pratt.png">
-      <img class="actorpics" src="./assets/img/rafe_spall.png">
-       <img class="actorpics" src="./assets/img/Toby_Jones.png">
-</div>
-
-</div>
-
-<a href="http://localhost/annexbios-cds2-leerdam/film" target="_blank"
-style="text-decoration: none;">
-<div id="ticketbox"><h1 style="font-size:2.5em; font-weight: 300;">KOOP JE TICKETS</h1></div>
+    <a href="http://localhost/annexbios-cds2-leerdam/film" target="_blank"
+    style="text-decoration: none;">
+    <div id="ticketbox">
+        <h1 style="font-size:2.5em; font-weight: 300;">KOOP JE TICKETS</h1>
+    </div>
 </a>
 
 <div id="movietrailer">
-    <iframe width="5000vw" height="100% " 
+    <iframe width="100%" height="100% "
     src="https://www.youtube.com/embed/vn9mMeWcgoM/?mute=1&loop=1&controls=1"></iframe>
 </div>
 
+</div>
 </div>
 
 
